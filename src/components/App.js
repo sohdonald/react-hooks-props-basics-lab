a39d64c13728a1f1b92bc27b6010cdeb01606908
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-08-15 16:44:19 -0400

    cleaned up

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
